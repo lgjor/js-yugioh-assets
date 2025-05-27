378dd02db3f7be2e47118e481bced098edd22cb7
Lucas Godoy <lucasgodoyjor@gmail.com>
2025-05-27 16:59:13 -0300

    Criadas funções para desenhar as cartas

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -19,8 +19,8 @@ const state = {
 };
 
 const playersSides = {
-    player1: "player-field-card",
-    computer: "computer-field-card",
+    player1: "player-cards",
+    computer: "computer-cards",
 }
 
 
@@ -53,13 +53,44 @@ const cardData = [
     },
 ];
 
+async function getRandomCardId() {
+    const randomIndex = Math.floor(Math.random() * cardData.length);
+    return cardData[randomIndex].id;
+}
+
+async function createCardImage(IdCard, fieldSide) {
+    const cardImage = document.createElement('img');
+    cardImage.setAttribute("height", "100px");
+    cardImage.setAttribute("src", "src/assets/icons/card-back.png");
+    cardImage.setAttribute("data-id", IdCard);
+    cardImage.classList.add("card");
+
+    if (fieldSide === playersSides.player1) {
+        cardImage.addEventListener('click', () =>{
+            setCardsField(cardImage.getAttribute("data-id"));
+        })
+    }
+
+    cardImage.addEventListener("mouseover", () => {
+        drawSelectedCard(IdCard);
+    });
+
+    return cardImage;    
+}
+
 async function drawCards(cardNumbers, fieldSide) {
-    for (card of cardNumbers) {
+    for (let i=0; i < cardNumbers; i++) {
         const radomIdCard = await getRandomCardId();
         const cardImage = await createCardImage(radomIdCard, fieldSide);
 
-        Document.getElementById(fieldSide).appendChild(cardImage);
+        document.getElementById(fieldSide).appendChild(cardImage);
     }
+}
+
+async function drawSelectedCard(IdCard) {
+    state.cardSprites.avatar.setAttribute("src", cardData[IdCard].img);
+    state.cardSprites.name.innerText = cardData[IdCard].name;
+    state.cardSprites.type.innerText = "Atribute: "+cardData[IdCard].type;
 }
 
 function init(){
